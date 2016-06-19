2c6aa34a53b74b94e57cdb7e407d7436bcb9cb12
shuaihanhungry <466629332@qq.com>
2016-06-19 09:42:49 +0800

    只爬活动资讯前9条

--- server/crawler.py
+++ server/crawler.py
@@ -1,23 +1,29 @@
 #coding=utf-8
+import database
 import urllib2
 from bs4 import BeautifulSoup
 
-homePage = "http://www.queshu.com"
 news = {} #活动：链接
 
-def newsFind():
+def news_find():
+    temp = {} #CAS
     try:
-        response = urllib2.urlopen(urllib2.Request(homePage))
+        home = "http://www.queshu.com"
+        response = urllib2.urlopen(urllib2.Request(home))
         soup = BeautifulSoup(response)
         for link in soup.find_all(class_="news_title_top", limit=9):
-            news[link.string] = homePage + link.parent["href"]
-        for key, value in news.items():
+            temp[link.string] = home + link.parent["href"]
+        for key, value in temp.items():
             response = urllib2.urlopen(urllib2.Request(value))
             soup = BeautifulSoup(response)
             for link in soup.find_all(class_="go_btn"):
                 if(link.parent["href"][1:5] == "link"):
-                    news[key] = homePage + link.parent["href"]
+                    temp[key] = home + link.parent["href"]
                 else:
-                    news[key] = link.parent["href"]
+                    temp[key] = link.parent["href"]
+        news = temp
     except urllib2.URLError, e:
         print(e.reason)
+
+def news_update():
+    pass
--- server/database.py
+++ server/database.py
@@ -1,25 +1,62 @@
 #coding=utf-8
-import MySQLdb
+import MySQLdb, json
 
-def user_info_create():
+def user_info_create(): #创建用户信息表
     db = MySQLdb.connect("localhost", "root", "admin", "app")
     cursor = db.cursor()
     sql = "drop table if exists user_info"
     cursor.execute(sql)
     sql = """create table user_info(
-            name varchar(10),
-            password varchar(14),
-            phone varchar(11),
-            school varchar(20))"""
+            username char(14),
+            password char(14),
+            phone char(11),
+            school char(50))
+            character set = utf8;"""
     cursor.execute(sql)
     db.close()
 
-def user_info_update(name, password, phone, school):
+def user_info_insert(username, password, phone, school): #更新用户信息，如果用户已经存在，则返回0
     db = MySQLdb.connect("localhost", "root", "admin", "app")
     cursor = db.cursor()
+    sql = 'select * from user_info where username="{0}"'.format(username)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if result:
+        return 0
+    try:
+        sql = 'insert into user_info values("{0}", "{1}", {2}, "{3}");'.format(username, password, phone, school)
+        print sql
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+    db.close()
 
-def book_info_create():
-    pass
+def user_info_select(username): #查询用户信息，返回结果为json格式，中文为utf-8编码
+    db = MySQLdb.connect("localhost", "root", "admin", "app")
+    cursor = db.cursor()
+    sql = 'select * from user_info where username="{0}"'.format(username)
+    cursor.execute(sql)
+    results = cursor.fetchone()
+    db.close()
+    username = results[0]
+    password = results[1]
+    phone = results[2]
+    school = results[3]
+    result = json.dumps({"username":username, "password":password, "phone":phone, "school":school}, ensure_ascii=False)
+    return result
 
-def book_info_update():
+def news_info_create():
+    db = MySQLdb.connect("localhost", "root", "admin", "app")
+    cursor = db.cursor()
+    sql = "drop table if exists book_info"
+    cursor.execute(sql)
+    sql = """create table news_info(
+            id int auto_increment not null primary key,
+            name varchar(14),
+            link varchar(20))"""
+    cursor.execute(sql)
+    db.close()
+
+def news_info_update():
     pass
