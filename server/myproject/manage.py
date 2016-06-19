ea6929b1cff1868ad6ad5a79ff826231dd65134f
shuaihanhungry <466629332@qq.com>
2016-06-19 18:57:25 +0800

    整合crawler模块，后台任务有问题

--- server/myproject/manage.py
+++ server/myproject/manage.py
@@ -5,6 +5,10 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
+    from myapp import crawler
+
+    crawler.start_crawler()
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
--- server/myproject/myapp/views.py
+++ server/myproject/myapp/views.py
@@ -1,11 +1,8 @@
 #coding:utf-8
 
 from django.http import HttpResponse
-from myapp.models import User
+from models import User
 import json
-
-def error(level):
-	return json.dumps({'error':level})
 
 def login(request):
 	if 'name' in request.GET and 'passwd' in request.GET:
@@ -14,10 +11,13 @@ def login(request):
 		if name and passwd:
 			user = User.objects.filter(name = name, passwd = passwd)
 			if user:
-				return HttpResponse(error('0')) #登录成功
+				result = {'status': '0', 'data': user.values()[0]}
+				return HttpResponse(json.dumps(result)) #登录成功
 			else:
-				return HttpResponse(error('1')) #登录无效
-	return HttpResponse(error('2')) #无效参数
+				result = {'status': '1', 'data': 'invalid username'}
+				return HttpResponse(json.dumps(result)) #登录无效
+	result = {'status': '2', 'data': 'invalid parameters'}
+	return HttpResponse(json.dumps(result)) #无效参数
 
 def register(request):
 	if 'name' in request.GET and request.GET['name']:
@@ -32,20 +32,19 @@ def register(request):
 						studentNo = request.GET['studentNo'], 
 						gender = request.GET['gender'])
 				u.save()
-				return HttpResponse(error('0')) #注册成功
+				result = {'status': '0', 'data': user.values()[0]}
+				return HttpResponse(json.dumps(result)) #注册成功
 			except:
-				return HttpResponse(error('1')) #无效参数
+				result = {'status': '2', 'data': 'invalid parameters'}
+				return HttpResponse(json.dumps(result)) #无效参数
 		else:
-			return HttpResponse(error('2')) #已经注册
-	return HttpResponse(error('1')) #无效参数
+			result = {'status': '1', 'data': 'this username has been registered'}
+			return HttpResponse(json.dumps(result)) #已经注册
+	result = {'status': '2', 'data': 'invalid parameters'}
+	return HttpResponse(json.dumps(result)) #无效参数
 
 def news(request):
 	return HttpResponse("news")
 
 def sale(request):
 	return HttpResponse("sale")
-
-'''
-u = User(name = 'shuaihan', passwd = 'admin', phone = '15682017891', school = 'uestc', email = '466629332@qq.com', studentNo = '201421010517', gender = '1')
-u.save()
-'''
