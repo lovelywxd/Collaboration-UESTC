cc3a69e87fa4e31f5103f414367a4af2e78e7025
Xiaodong Wang <409681075@qq.com>
2016-07-02 12:24:11 +0800

    modify

--- server/webApp/YSGApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-# from django.contrib.auth.models import User
-
-from YSGApp.models import User
-# Register your models here.
-
-# Re-register UserAdmin
-admin.site.register(User)
--- server/webApp/YSGApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class YsgappConfig(AppConfig):
-    name = 'YSGApp'
